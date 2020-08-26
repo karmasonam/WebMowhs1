c26a16b120c5c9bddc3eb49ca54198401ca38944
Ubuntu <ksonammowhs@ksonammowhs.1nded2iooz1ezdmnl5v4mugmue.gx.internal.cloudapp.net>
2020-08-26 05:22:35 +0000

    commit

---
+++ src/main/java/gov/bt/MowhsWeb1/MowhsWeb1Application.java
@@ -0,0 +1,13 @@
+package gov.bt.MowhsWeb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowhsWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MowhsWeb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/gov/bt/MowhsWeb1/MowhsWeb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.bt.MowhsWeb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MowhsWeb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
